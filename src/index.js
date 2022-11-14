043f806df7d7e2a36805147e9be2d73399fe119b
jaime-ribeiro <littlejou@gmail.com>
2022-11-14 19:18:24 -0300

    Create user :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // importação express
 const express = require('express');
+const {v4: uuidv4} = require("uuid"); // renomeando para uuidv4
 
 const app = express();
+
+const customers = [];
 
 app.use(express.json());//midware possibilitando esperar um json
 
@@ -13,12 +16,27 @@ app.get("/courses", (request, response) => {
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
-app.post("/courses",(request,response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-}
-)
+app.post("/account", (request, response)=>{
+    const {cpf,name} = request.body;
+
+    const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+    if(customersAlreadyExists){
+        return response.status(400).json({error: "Customer Already Exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    });
+
+    //gerar uuid dentro do objeto
+
+    return response.status(201).send();
+    //returnando o status code 201 que confirma que deu tudo certo
+});
 
 app.put("/courses/:id", (request,response)=>{
     const {id} = request.params;
