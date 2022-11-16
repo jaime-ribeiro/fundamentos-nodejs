27c63afc42be6e1c8ca4813f444278138ed77576
jaime-ribeiro <littlejou@gmail.com>
2022-11-16 00:03:05 -0300

    Create new middleware and Route :sparkles:

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ const {v4: uuidv4} = require("uuid"); // renomeando para uuidv4
 
 const app = express();
 
-const customers = [];
-
 app.use(express.json());//midware possibilitando esperar um json
-
 // localhost:3333
 
-//extrato bancário "statement"
-app.get("/statement/:cpf", (request, response) => {
-    const {cpf} = request.params;
+const customers = [];
+
+function verifyIfExistsAccountCPF(request, response, next) {
+    const {cpf} = request.headers;
+
     const customer = customers.find(customer => customer.cpf === cpf);
-    return response.json(customer.statement);
-});
+
+    if(!customer){
+        return response.status(400).json({error: "Costumer not found"});
+    }
+    //criação de um request para que as rotas tenham acesso
+    request.customer = customer;
+
+    return next();
+}
 
 app.post("/account", (request, response)=>{
     const {cpf,name} = request.body;
@@ -37,6 +43,33 @@ app.post("/account", (request, response)=>{
 
     return response.status(201).send();
     //returnando o status code 201 que confirma que deu tudo certo
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) =>{
+    const {description, amount} = request.body;
+    
+    const {customer} = request;
+
+    //O tipo crédito é deposito, débito saque
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+});
+
+app.use(verifyIfExistsAccountCPF);
+
+//extrato bancário "statement"
+//Middleware utilizado após o nome da rota
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+    //fazendo a desestruturação do request criado dentro do middleware
+    const {customer} = request;
+    return response.json(customer.statement);
 });
 
 app.put("/courses/:id", (request,response)=>{
